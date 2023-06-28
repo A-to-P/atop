1f3c3b9129fb7a774a6efbfa22484b5fc98f5439
gpffh20 <eunbin584836@gmail.com>
2023-06-28 20:11:15 +0900

    Feat: 로그아웃 기능 추가

--- account/urls.py
+++ account/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('login', views.login, name="login"),
     path('signup', views.signup, name="signup"),
+    path('logout', views.logout, name="logout"),
     path('signup_restaurant', views.signup_restaurant, name="signup_restaurant"),
     path('signup_consultant', views.signup_consultant, name="signup_consultant"),
     
