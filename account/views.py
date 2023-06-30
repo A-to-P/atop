635b72474859562b811f43de0593727945f94a53
yunseul <yyd2798@kookmin.ac.kr>
2023-07-01 00:22:44 +0900

    feat: fix signup

--- account/views.py
+++ account/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import authenticate,login as auth_login, logout as auth_logout
 from django.shortcuts import render, redirect
 from .models import User
+from django.contrib import auth
 
 # Create your views here.
 
@@ -34,13 +35,57 @@ def signup(request):
 def signupFrom(request):
     return render(request, 'signupForm.html')
 
-# 요식업자 회원가입 폼
 def signup_restaurant(request):
-    return render(request, 'signup_restaurant.html') 
+    # if request.method == "GET":
+    #     return render(request, 'signup_restaurant.html')
 
-# 컨설턴트 회원가입 폼
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        password_check = request.POST['password_check']
+        email = request.POST['email']
+
+        if not username or not password:
+            error_username = '아이디와 비밀번호를 모두 입력해주세요.'
+            return render(request, 'account/signup_restaurant.html', {'error_username': error_username})
+        if User.objects.filter(username=username).exists():
+            error_username = '이미 사용중인 아이디입니다.'
+            return render(request, 'account/signup_restaurant.html', {'error_username': error_username})
+        if password != password_check:
+            error_password = '비밀번호와 비밀번호 확인이 일치하지 않습니다.'
+            return render(request, 'account/signup_restaurant.html', {'error_password': error_password})
+        else:   
+            user = User.objects.create_normaluser(username=username, password=password)
+
+            user = User(user=user)
+            user.save()
+
+            auth.login(request,user)
+            return render('home')
+    return render(request, 'signup_restaurant.html')
+
+#컨설턴트 회원가입폼
 def signup_consultant(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        password_check = request.POST['password_check']
+        email = request.POST['email']
+
+        if not username or not password:
+            error_username = '아이디와 비밀번호를 모두 입력해주세요.'
+            return render(request, 'account/signup_consultant.html', {'error_username': error_username})
+        if User.objects.filter(username=username).exists():
+            error_username = '이미 사용중인 아이디입니다.'
+            return render(request, 'account/signup_consultant.html', {'error_username': error_username})
+        if password != password_check:
+            error_password = '비밀번호와 비밀번호 확인이 일치하지 않습니다.'
+            return render(request, 'account/signup_consultant.html', {'error_password': error_password})
+        else:   
+            user = User.objects.create_normaluser(username=username, password=password)
+            user = User(user=user)
+            user.save()
+
+            auth.login(request,user)
+            return render('home')
     return render(request, 'signup_consultant.html')
-
-
-    
