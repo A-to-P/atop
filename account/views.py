d1046e9ca0ef5d4e57d85324bfb010a5ce62c78b
haram8009 <haram8009@gmail.com>
2023-07-03 21:59:58 +0900

    Feat: 요식업자 회원가입

--- account/views.py
+++ account/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth import authenticate,login as auth_login, logout as auth_logout
 from django.shortcuts import render, redirect
-from .models import User, Tag
+from .models import User, Tag, ConsultantProfile, RestaurantProfile
 from django.contrib import auth
 
 # Create your views here.
@@ -37,28 +37,35 @@ def signupForm(request):
 
 # 요식업자 폼
 def signup_restaurant(request):
+    tags = list(Tag.objects.filter(job="restaurant").values())
+    if request.method =="GET":
+        return render(request, 'signup_restaurant.html', {'tags':tags})
+    
     if request.method == "POST":
+        tag_value = request.POST['restaurant_field']
         username = request.POST['username']
         password = request.POST['password']
         password_check = request.POST['password_check']
         email = request.POST['email']
+        
 
         if not username or not password:
             error_username = '아이디와 비밀번호를 모두 입력해주세요.'
-            return render(request, 'signup_restaurant.html', {'error': error_username})
+            return render(request, 'signup_restaurant.html', {'error': error_username, 'tags':tags})
         if User.objects.filter(username=username).exists():
             error_username = '이미 사용중인 아이디입니다.'
-            return render(request, 'signup_restaurant.html', {'error': error_username})
+            return render(request, 'signup_restaurant.html', {'error': error_username, 'tags':tags})
         if password != password_check:
             error_password = '비밀번호와 비밀번호 확인이 일치하지 않습니다.'
-            return render(request, 'signup_restaurant.html', {'error': error_password})
+            return render(request, 'signup_restaurant.html', {'error': error_password, 'tags':tags})
         else:   
-            user = User(username=username,password=password,email=email,job='restaurant')
-            user.save()
-
+            tag = Tag.objects.get(value=tag_value)
+            user = User.objects.create(username=username,password=password,email=email,job='restaurant')
+            user.tag.add(tag.id)
+            
             auth.login(request,user)
             return redirect('home')
-    return render(request, 'signup_restaurant.html')
+    
 
 #컨설턴트 회원가입폼
 def signup_consultant(request):
@@ -84,9 +91,3 @@ def signup_consultant(request):
             auth.login(request,user)
             return redirect('home')
     return render(request, 'signup_consultant.html')
-
-def restaurant_field(request):
-    name = request.GET.get('restaurant_field')
-    value = Tag.objects.filter(restaurant_field=name)    
-    job = request.GET.get('signup_restaurant')
-    return render(request, 'signup_restaurant.html', {'restaurant_field' : restaurant_field})
