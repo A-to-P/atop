1006a3fd1a56ff160c66e17b4dd384e8f3c3bd8d
gpffh20 <eunbin584836@gmail.com>
2023-07-03 12:43:16 +0900

    Feat: 프로필 기능 추가 컨설턴트 기본 프로필 기능만 추가 요식업자 프로필 html, 프로필 수정 html 만들어지면 기능 추가 필요

--- myProfile/views.py
+++ myProfile/views.py
@@ -1,9 +1,13 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.db import models
+from account.models import RestaurantProfile, ConsultantProfile, User
 # Create your views here.
 
 def myProfile(request):
-    return render(request, "myProfile.html")
+    if request.user.is_anonymous:
+        return redirect('login')
+    profile, created = ConsultantProfile.objects.get_or_create(user=request.user)
+    return render(request, "myProfile.html", {'profile' : profile})
 
 def editMyprofile(request):
     return render(request, "editMyprofile.html")
