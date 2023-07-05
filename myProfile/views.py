f6c3801b333217dff5aa9e3d1ef05601136a0c6b
gpffh20 <eunbin584836@gmail.com>
2023-07-05 17:26:50 +0900

    Feat: 요식업자 프로필 html 학력 -> 경력으로 수정 오류 주석처리

--- myProfile/views.py
+++ myProfile/views.py
@@ -1,17 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.db import models
+from django.contrib.auth.decorators import login_required
+from account.models import RestaurantProfile, ConsultantProfile, User
 
 # Create your views here.
 
 def consultantProfile(request):
     return render(request, "consultantProfile.html")
+
+@login_required
 def restaurantProfile(request):
-    return render(request, "restaurantProfile.html")
+    res_profile, created = RestaurantProfile.objects.get_or_create(user=request.user)
+    return render(request, "restaurantProfile.html", {'res_profile' : res_profile})
 
 def editConsultProfile(request):
     return render(request, "editConsultProfile.html")
 
 def editRestProfile(request):
-    return render(request, "editRestProfile.html")
+    res_profile, created = RestaurantProfile.objects.get_or_create(user=request.user)
+    if request.method == "POST":
+
+        res_profile.name = request.POST.get('name')
+        res_profile.image = request.FILES.get('profile_image')
+        User.email = request.POST.get('email')
+        res_profile.birth = request.POST.get('age')
+        res_profile.career = request.POST.get('career')
+        # 밑에 두 개 "IntegrityError, Not NULL constraint failed" 
+        # res_profile.self_introducing = request.POST.get('introduction')
+        # res_profile.contact_at = request.POST.get('inputGroup-sizing-default')
+        res_profile.menu = request.POST.get('sig_menu')
+        res_profile.location = request.POST.get('location')
+        res_profile.area = request.POST.get('size')
+        res_profile.save()
+        return redirect('restaurantProfile')
+    return render(request, "editRestProfile.html", {'res_profile' : res_profile})
 
 def profile_template(request):
     return render(request, "profile_template.html")
