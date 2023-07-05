0f2cc9b9b4990273d9b6881da9074d468c4a457c
haram8009 <haram8009@gmail.com>
2023-07-06 00:20:49 +0900

    컨설팅 모델 수정

--- consulting/views.py
+++ consulting/views.py
@@ -2,11 +2,14 @@ from django.shortcuts import render
 
 # Create your views here.
 
+
 def myConsulting(request):
     return render(request, 'myConsulting.html')
 
+
 def consultingSpace(request):
     return render(request, 'consultingSpace.html')
+
 
 def consultingHistory(request):
     return render(request, 'consultingHistory.html')
