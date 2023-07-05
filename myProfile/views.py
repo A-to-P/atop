c8c3fe170f1e1e1cb5b32ae53c61482cd92684fe
gpffh20 <eunbin584836@gmail.com>
2023-07-05 22:21:47 +0900

    Feat: 컨설턴트 프로필

--- myProfile/views.py
+++ myProfile/views.py
@@ -5,8 +5,10 @@ from account.models import RestaurantProfile, ConsultantProfile, User
 
 # Create your views here.
 
+@login_required
 def consultantProfile(request):
-    return render(request, "consultantProfile.html")
+    con_profile, created = ConsultantProfile.objects.get_or_create(user=request.user)
+    return render(request, "consultantProfile.html", {'con_profile' : con_profile})
 
 @login_required
 def restaurantProfile(request):
@@ -14,7 +16,23 @@ def restaurantProfile(request):
     return render(request, "restaurantProfile.html", {'res_profile' : res_profile})
 
 def editConsultProfile(request):
-    return render(request, "editConsultProfile.html")
+    con_profile, created = ConsultantProfile.objects.get_or_create(user=request.user)
+    user = request.user
+    if request.method == "POST":
+        con_profile.name = request.POST.get('name')
+        con_profile.image = request.FILES.get('profile_image')
+        User.email = request.POST.get('email')
+        con_profile.birth = request.POST.get('age')
+        con_profile.education = request.POST.get('school') 
+        con_profile.self_introducing = request.POST.get('introduction')
+        # con_profile.contact_at = request.POST.get('inputGroup-sizing-default')
+        con_profile.save()
+        # 컨설팅 분야
+
+        # 컨설팅 횟수
+
+        return redirect('consultantProfile')
+    return render(request, "editConsultProfile.html", {'con_profile' : con_profile})
 
 def editRestProfile(request):
     res_profile, created = RestaurantProfile.objects.get_or_create(user=request.user)
