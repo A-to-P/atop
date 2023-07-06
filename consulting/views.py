5e2a74dcc0de598373d44f2ef16909ba7003f015
gpffh20 <eunbin584836@gmail.com>
2023-07-06 22:13:01 +0900

    fix: paginator 에러 수정

--- consulting/views.py
+++ consulting/views.py
@@ -22,11 +22,9 @@ def consultingHistory(request):
         history_list = Consulting.objects.filter(restaurant=user, done=True)
         result = []
         paginator = Paginator(history_list, 8) # 한 페이지에 최대 8개
-        page_number = request.GET.get('page')
+        page_number = int(request.GET.get('page', 1))
         page_obj = paginator.get_page(page_number)
-        # print(paginator.count, paginator.num_pages )
         for history in page_obj:
-            print('history:', history)
             tmp = {}
             tmp['consulting_id']=history.id
             tmp['end']=history.end
@@ -37,9 +35,9 @@ def consultingHistory(request):
         return render(request, 'consultingHistory.html',
                       {'history_list' :result,
                        "page_number":page_number,
-                       'paginator':{'num_pages':paginator.num_pages, 'page_number':int(page_number)}})
+                       'paginator':{'num_pages':paginator.num_pages, 'page_number':page_number}})
     
-    # return render(request, 'consultingHistory.html')
+    return render(request, 'consultingHistory.html')
 
 # 컨설턴트의 포트폴리오 페이지
 def consultingPortfolio(request):
