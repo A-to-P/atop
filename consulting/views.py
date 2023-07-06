d3b3a3021865be4a06305775c72e021c41450b2a
haram8009 <haram8009@gmail.com>
2023-07-06 19:37:12 +0900

    Feat: 컨설팅 히스토리 페이지네이션 완성

--- consulting/views.py
+++ consulting/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .models import Consulting
+from django.core.paginator import Paginator
 # Create your views here.
 
 
@@ -11,8 +13,33 @@ def consultingSpace(request):
     return render(request, 'consultingSpace.html')
 
 # 요식업자의 포트폴리오 페이지
+@login_required
 def consultingHistory(request):
-    return render(request, 'consultingHistory.html')
+    user = request.user
+    if user.job != 'restaurant':
+        return redirect('home')
+    if request.method =="GET":
+        history_list = Consulting.objects.filter(restaurant=user, done=True)
+        result = []
+        paginator = Paginator(history_list, 8) # 한 페이지에 최대 8개
+        page_number = request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
+        # print(paginator.count, paginator.num_pages )
+        for history in page_obj:
+            print('history:', history)
+            tmp = {}
+            tmp['consulting_id']=history.id
+            tmp['end']=history.end
+            tmp['consultant']=history.consultant.name
+            tmp['tag']=history.tags[0].name
+            tmp['fee']=history.fee
+            result.append(tmp)
+        return render(request, 'consultingHistory.html',
+                      {'history_list' :result,
+                       "page_number":page_number,
+                       'paginator':{'num_pages':paginator.num_pages, 'page_number':int(page_number)}})
+    
+    # return render(request, 'consultingHistory.html')
 
 # 컨설턴트의 포트폴리오 페이지
 def consultingPortfolio(request):
