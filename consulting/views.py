c2a89e89cd518cf979c78bbdbee819ccc6d6225b
gpffh20 <eunbin584836@gmail.com>
2023-07-07 10:56:12 +0900

    Feat: 신고 기능

--- consulting/views.py
+++ consulting/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .models import Consulting, Accusation, Review
 from account.models import User
+from django.http import HttpResponse
 from django.core.paginator import Paginator
 # Create your views here.
 
@@ -49,7 +50,6 @@ def consultingPortfolio(request):
 
 # 신고
 def accuse(request):
-    print(1)
     if request.method == "POST":
         user = request.user
         if user.job == "restaurant":
@@ -58,8 +58,8 @@ def accuse(request):
             accusation.complainant = request.user
             accusation.defendant = consulting.consultant
             accusation.evidence = request.FILES.get('declaration-info')
-            accusation.comment = request.POST.get('declaration-content')
+            accusation.comment = request.POST.get('declaration_content')
             accusation.save()
 
-        return redirect('myConsulting')
+        return HttpResponse('ok')
 
